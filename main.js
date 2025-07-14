126d91de72e269896b21cce7d16f35fb1a7c26b3
Eric Whittaker <eric.whittakerjr@gmail.com>
2025-07-14 14:24:42 -0400

    Enhance auto-updater notifications and implement application menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow, ipcMain, Menu, dialog } = require('electron/main')
 const { autoUpdater } = require('electron-updater')
 const packageJson = require('./package.json')
 
@@ -16,6 +16,13 @@ autoUpdater.on('checking-for-update', () => {
 
 autoUpdater.on('update-available', (info) => {
   console.log('Update available:', info.version)
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Available',
+    message: `A new version is available!`,
+    detail: `Version ${info.version} is downloading in the background. You'll be notified when it's ready to install.`,
+    buttons: ['OK']
+  })
 })
 
 autoUpdater.on('update-not-available', (info) => {
@@ -33,8 +40,18 @@ autoUpdater.on('download-progress', (progressObj) => {
 
 autoUpdater.on('update-downloaded', (info) => {
   console.log('Update downloaded')
-  // Auto-install on next restart
-  autoUpdater.quitAndInstall()
+  // Show dialog to user about update
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Ready',
+    message: 'Update has been downloaded and will be installed on restart.',
+    detail: `Version ${info.version} is ready to install.`,
+    buttons: ['Restart Now', 'Later']
+  }).then((result) => {
+    if (result.response === 0) {
+      autoUpdater.quitAndInstall()
+    }
+  })
 })
 
 const createWindow = () => {
@@ -69,6 +86,169 @@ const createWindow = () => {
   // win.loadURL('http://platform.onelife.app')
 }
 
+// Create application menu
+const createMenu = () => {
+  const template = [
+    {
+      label: 'EventRunner',
+      submenu: [
+        {
+          label: 'About EventRunner',
+          click: async () => {
+            const version = app.getVersion()
+            dialog.showMessageBox({
+              type: 'info',
+              title: 'About EventRunner',
+              message: 'EventRunner',
+              detail: `Version: ${version}\nEvent Management Software\n\nBuilt with Electron and Angular`,
+              buttons: ['OK']
+            })
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'Preferences...',
+          accelerator: 'CmdOrCtrl+,',
+          enabled: false // TODO: Implement preferences
+        },
+        { type: 'separator' },
+        {
+          label: 'Hide EventRunner',
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Alt+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        { type: 'separator' },
+        {
+          label: 'Quit EventRunner',
+          accelerator: 'CmdOrCtrl+Q',
+          click: () => app.quit()
+        }
+      ]
+    },
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New Event',
+          accelerator: 'CmdOrCtrl+N',
+          enabled: false // TODO: Implement new event
+        },
+        { type: 'separator' },
+        {
+          label: 'Close Window',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'close' }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Check for Updates...',
+          click: async () => {
+            console.log('Manual update check triggered')
+            // Show checking dialog
+            const checkingDialog = dialog.showMessageBox({
+              type: 'info',
+              title: 'Checking for Updates',
+              message: 'Checking for updates...',
+              buttons: ['Cancel'],
+              defaultId: 0
+            })
+            
+            try {
+              const result = await autoUpdater.checkForUpdates()
+              // Close the checking dialog
+              checkingDialog.then((dialogResult) => {
+                // Dialog was closed
+              })
+              
+              if (!result.updateInfo || result.updateInfo.version === app.getVersion()) {
+                dialog.showMessageBox({
+                  type: 'info',
+                  title: 'No Updates Available',
+                  message: 'You are running the latest version.',
+                  detail: `Current version: ${app.getVersion()}`,
+                  buttons: ['OK']
+                })
+              }
+            } catch (error) {
+              console.error('Update check failed:', error)
+              dialog.showMessageBox({
+                type: 'error',
+                title: 'Update Check Failed',
+                message: 'Failed to check for updates.',
+                detail: error.message,
+                buttons: ['OK']
+              })
+            }
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'EventRunner Help',
+          click: () => {
+            // TODO: Open help documentation
+            dialog.showMessageBox({
+              type: 'info',
+              title: 'Help',
+              message: 'EventRunner Help',
+              detail: 'Help documentation coming soon!',
+              buttons: ['OK']
+            })
+          }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
+
 app.whenReady().then(() => {
   ipcMain.handle('ping', () => 'pong')
   
@@ -92,6 +272,9 @@ app.whenReady().then(() => {
     console.log('Version requested, returning:', version);
     return version;
   });
+  
+  // Create the application menu
+  createMenu()
   
   createWindow()
 
