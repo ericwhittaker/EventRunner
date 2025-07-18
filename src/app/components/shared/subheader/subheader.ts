f9152b1c3a9e91be1260b1ed44ab0946c5288cbe
Eric Whittaker <eric.whittakerjr@gmail.com>
2025-07-18 16:52:08 -0400

    feat: add Subheader component with template and styles refactor: update menu item label from 'Trips / Vehicles' to 'Events'

---
+++ src/app/components/shared/subheader/subheader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'er-subheader',
+  imports: [],
+  templateUrl: './subheader.html',
+  styleUrl: './subheader.scss'
+})
+export class Subheader {
+
+}
