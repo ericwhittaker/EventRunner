12dca0c368eb75b69bbd4ce7a3784082d404b9cc
Eric Whittaker <eric.whittakerjr@gmail.com>
2025-07-25 23:23:42 -0400

    refactor: add Auth0 provider configuration to auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,5 +4,9 @@ export default {
       domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      applicationID: process.env.AUTH0_CLIENT_ID,
+    }
   ],
 };
